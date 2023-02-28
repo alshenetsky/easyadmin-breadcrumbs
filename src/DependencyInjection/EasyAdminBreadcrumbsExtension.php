75e4f6eb8aa051609c32404dad5f851a8d408526
Alexey <alshenestky@icloud.com>
2023-02-28 05:45:15 +0300

    add services.yaml

--- src/DependencyInjection/EasyAdminBreadcrumbsExtension.php
+++ src/DependencyInjection/EasyAdminBreadcrumbsExtension.php
@@ -4,8 +4,10 @@ namespace Alshenetsky\EasyAdminBreadcrumbs\DependencyInjection;
 
 use Alshenetsky\EasyAdminBreadcrumbs\Breadcrumb\Breadcrumbs;
 use Alshenetsky\EasyAdminBreadcrumbs\Twig\BreadcrumbsExtension;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class EasyAdminBreadcrumbsExtension extends Extension
 {
@@ -15,5 +17,11 @@ class EasyAdminBreadcrumbsExtension extends Extension
             ->addTag('easyadmin-breadcrumbs-bundle');
         $container->registerForAutoconfiguration(BreadcrumbsExtension::class)
             ->addTag('twig.extension');
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yaml');
     }
 }
--- src/EasyAdminBreadcrumbsBundle.php
+++ src/EasyAdminBreadcrumbsBundle.php
@@ -6,8 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EasyAdminBreadcrumbsBundle extends Bundle
 {
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
 }
